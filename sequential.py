081d6a20e4560c16e85ced949463237ce6c8557c
Masood <masoodak000@gmail.com>
2026-08-11 18:33:51 +0100

    initial commit: created sequential.py which makes requests to an API sequentially

---
+++ sequential.py
@@ -0,0 +1,28 @@
+import requests
+import time
+
+
+def fetch(delay: int) -> str:
+    url = f"https://httpbin.org/delay/{delay}"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()["url"]
+
+def main() -> None:
+    start = time.perf_counter()
+    delays = [0,1,2,3,4,5,5]
+    output = []
+    for number,delay in enumerate(delays, start = 1):
+        result = fetch(delay)
+        elapsed = time.perf_counter() - start
+        output.append(result)
+        print(f"{number} request returned in {elapsed:.4f} seconds")
+    print(output)
+
+    total = time.perf_counter() - start
+    print(f"Script total run time: {total:.4f} seconds")
+
+
+
+if __name__ == "__main__":
+    main()
