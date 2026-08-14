266b4640da241940b46f5a3a98918b4f1101945b
Masood <masoodak000@gmail.com>
2026-08-14 18:29:36 +0100

    moved timing print outs into fetch func, fetch now owns the entire lifecycle of a request in sequential.py

--- sequential.py
+++ sequential.py
@@ -3,27 +3,37 @@ import time
 import threading
 
 def fetch(delay: int) -> str:
+    start = time.perf_counter()
+
+    print(
+        f"{threading.current_thread().name} "
+        f"started delay = {delay}"
+    )
+
     url = f"http://localhost:8080/delay/{delay}"
     response = requests.get(url)
     response.raise_for_status()
+    
+    print(
+        f"{threading.current_thread().name} "
+        f"finished delay = {delay}, "
+        f"time taken = {time.perf_counter() - start:.4f} seconds"
+    )
+
     return response.json()["url"]
 
 def main() -> None:
     start = time.perf_counter()
     delays = [0,1,2,3,4,5,6]
     output = []
-    for number,delay in enumerate(delays, start = 1):
-        result = fetch(delay)
-        elapsed = time.perf_counter() - start
-        output.append(result)
-        print(f"Starting delay={delay} on {threading.current_thread().name }"
-              f"{number} request returned in {elapsed:.4f} seconds ")
-    print(output)
 
+    for delay in delays:
+        result = fetch(delay)
+        output.append(result)
+
+    print(output)
     total = time.perf_counter() - start
     print(f"Script total run time: {total:.4f} seconds")
-
-
 
 if __name__ == "__main__":
     main()
