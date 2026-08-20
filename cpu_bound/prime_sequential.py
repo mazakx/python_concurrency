b329a17183a952a56c7af2147316881487be650e
Masood <masoodak000@gmail.com>
2026-08-20 14:21:37 +0100

    added thread print outs to prime_sequential.py to show what thread is executing

--- cpu_bound/prime_sequential.py
+++ cpu_bound/prime_sequential.py
@@ -1,12 +1,15 @@
 import math
 import time
+import threading
 
 def is_prime(n: int) -> bool:
     if n < 2:
         return False
+
     for divisor in range(2, math.isqrt(n) + 1):
         if n % divisor == 0:
             return False
+
     return True
 
 if __name__ == "__main__":
@@ -26,8 +29,9 @@ if __name__ == "__main__":
         result = is_prime(number)
 
         print(
+            f"{threading.current_thread().name} | "
             f"{number} | prime = {result} | "
             f"Time taken: {time.perf_counter() - item_start:.4f}s"
         )
 
-    print(f"Total runtime: {time.perf_counter() - start:.4f}s")
+    print(f"Total script runtime: {time.perf_counter() - start:.4f}s")
